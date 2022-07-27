e72b7f1c1b6ef6ed2f5596fbc3b0530bb3de5b9d
Dayt0w <hallman.alec13@gmail.com>
2022-07-27 13:58:43 -0400

    Fixed
    Somehow everything broke idk now its back to normal

---
+++ Assets/Scripts/Plants/PlantBase1.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlantBase1 : MonoBehaviour
+{
+    public int startHealth;
+    public int damage { get { return 0; } }
+    public int health;
+    public float damageSpeed;
+
+    void Start()
+    {
+        health = startHealth;
+    }
+
+
+
+    public void TakeDamage(int damage)
+    {
+        // Debug.Log("Ouch!");
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Plants/ShooterPlantBase1.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterPlantBase1 : PlantBase
+{
+    private bool WaitFrame;
+    private bool repeat;
+    private bool shootAgain;
+    private float RepeaterOffset = 0.2f;
+    private float startTime;
+    public GameObject pea;
+    void Start()
+    {
+        health = startHealth;
+        startTime = Time.realtimeSinceStartup;
+
+    }
+
+    void Update()
+    {
+
+
+        string name = gameObject.name;
+        if (name.Contains("Rep"))
+        {
+            repeat = true;
+        }
+        Ray detectZomb = new Ray(transform.position, transform.forward);
+        float currentTime = Time.realtimeSinceStartup - startTime;
+        WaitFrame = true;
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+
+        foreach (RaycastHit hit in Physics.RaycastAll(detectZomb))
+        {
+
+            if (hit.collider.gameObject.tag == "Zombie")
+            {
+                if (currentTime > damageSpeed)
+                {
+                    startTime = Time.realtimeSinceStartup;
+                    Shoot();
+                    shootAgain = true;
+                    WaitFrame = false;
+
+                }
+                if (repeat == true && RepeaterOffset < currentTime && shootAgain == true && WaitFrame == true)
+                {
+                    Shoot();
+                    shootAgain = false;
+                    startTime = Time.realtimeSinceStartup;
+
+                }
+            }
+
+
+        }
+
+    }
+    public void Shoot()
+    {
+        GameObject newPea = Instantiate(pea);
+        Rigidbody peaBody = newPea.GetComponent<Rigidbody>();
+        Vector3 pos = transform.position;
+        newPea.transform.position = new Vector3(pos.x + 0.5f, pos.y + 0.1f, pos.z);
+        peaBody.AddForce(new Vector3(0.3f, 0, 0), ForceMode.Impulse);
+    }
+
+
+}
