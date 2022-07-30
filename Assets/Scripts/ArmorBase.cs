b6614ac1fbafce65bdc85c8b2cb1fa82ca194282
Dayt0w <hallman.alec13@gmail.com>
2022-07-30 15:43:55 -0400

    Nutt Damage

--- Assets/Scripts/ArmorBase.cs
+++ Assets/Scripts/ArmorBase.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ArmorBase : PlantBase1
 {
+    GameObject hurt2;
+    GameObject hurt1;
+    private bool exists = false;
+    private bool exists2 = false;
     public GameObject Damage1;
     public GameObject Damage2;
     // Start is called before the first frame update
@@ -15,9 +19,30 @@ public class ArmorBase : PlantBase1
     // Update is called once per frame
     void Update()
     {
-        if(dead == true)
+        Debug.Log(health);
+        if (dead == true)
         {
+            Destroy (hurt2);
             Destroy(this.gameObject);
+        }
+        if (health < 4800 && health > 2700 && exists == false)
+        {
+            exists = true;
+            hurt1 = Instantiate(Damage1);
+            hurt1.transform.position = transform.position;
+            hurt1.transform.rotation = transform.rotation;
+            foreach (var r in GetComponentsInChildren<Renderer>())
+            {
+                r.enabled = false;
+            }
+        }
+        if (health < 2700 && exists2 == false)
+        {
+            Destroy(hurt1);
+            exists2 = true;
+            hurt2 = Instantiate(Damage2);
+            hurt2.transform.position = transform.position;
+            hurt2.transform.rotation = transform.rotation;
         }
     }
 }
